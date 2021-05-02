4b853710fe5ad94b1db4f30796e81f724594d67d
bjaan <42038171+bjaan@users.noreply.github.com>
2021-05-03 01:20:59 +0200

    Working implementation of Airplay status detection - (dis)connect, shairport (in)active

--- AMPi-Node/app.js
+++ AMPi-Node/app.js
@@ -1,19 +1,79 @@
 let SerialPort = require('serialport');
 let ShairportReader = require('shairport-sync-reader');
+const { exec } = require("child_process");
+
+let shairportActive = false;
+let shairportOpen = false;
+const MAX_MESSAGE = 200;
+const MAX_TEXT = 35;
+
+// Utility Functions
+
+function serialSend(buf, success) {
+	serial0.write(buf, (err) => {
+		if (err) { console.log('Error on write: ', err.message); return; }
+		success();
+	});
+}
+function serialSendStatus(text) {
+	let textBuf = Buffer.from(text, 'ascii');
+	let buf = Buffer.alloc(textBuf.length+4);
+	textBuf.copy(buf, 3);
+	buf[0] = 60; // < 3C
+	buf[1] = 83; // S 53
+	buf[2] = textBuf.length;
+	buf[buf.length-1] = 62; // > 3E
+	console.log(buf);
+	serialSend(buf, () => console.log('Status "' + text + '" sent'));
+}
+
+// Service Monitor
+
+function checkShairport() {
+	exec("service shairport-sync status | grep inactive", (error, stdout, stderr) => {
+		if (stdout.includes("inactive")) {
+			if (shairportActive /*only once*/) {
+				console.log("shairport down");
+				serialSend(Buffer.from('<R\x03\xFF\x8C\x1A>', 'ascii'), () => console.log('Airplay RED sent'));
+				shairportOpen = false;
+			}
+			shairportActive = false;
+		} else {
+			if (!shairportOpen /*when not connected*/ && !shairportActive /*only once*/) {
+				console.log("shairport up");
+				serialSend(Buffer.from('<R\x03\xFF\xFF\xFF>', 'ascii'), () => console.log('Airplay WHITE sent'));
+			}
+			shairportActive = true;
+		}
+	});
+}
+
+setInterval(function() {
+	checkShairport();
+}, 5000);
 
 // Shairport Sync Management
+
 let shairPort = new ShairportReader({ path: '/tmp/shairport-sync-metadata' });
 shairPort.on('pbeg',readShairPortBegin);
+shairPort.on('pend',readShairPortEnd);
 
 function readShairPortBegin(data) {
-  console.log("shairport pbeg " + JSON.stringify(data));
-  serial0.write('\x7ER\xDC\x13\x7F', function(err) {
-  if (err) {
-        console.log('Error on write: ', err.message);
-        return;
-  }
-  console.log('Airplay BLUE sent');
-})
+	console.log("shairport pbeg " + JSON.stringify(data));
+	serialSend(Buffer.from('<R\x03\x10\x79\xE6>', 'ascii'), () => {
+		shairportOpen = true;
+		console.log('Airplay BLUE sent');
+	});
+	serialSendStatus("> " + data["Client-IP"]);
+}
+
+function readShairPortEnd(data) {
+	console.log("shairport pend " + JSON.stringify(data));
+	serialSend(Buffer.from('<R\x03\xFF\xFF\xFF>', 'ascii'), () => {
+		shairportOpen = false;
+		console.log('Airplay WHITE sent');
+	});
+	serialSendStatus("");
 }
 
 // Serial Management
@@ -26,25 +86,15 @@ serial0.on('close', showPortClose);
 serial0.on('error', showError);
 
 function showPortOpen() {
-  console.log('port open. Data rate: ' + serial0.baudRate);
-  serial0.write('\x7EP\x7F', function(err) {
-  if (err) {
-  	console.log('Error on write: ', err.message);
-	return;
-  }
-  console.log('Power On (P) sent')
-})
+	console.log('port open. Data rate: ' + serial0.baudRate);
+	serialSend(Buffer.from('<P>', 'ascii'), () => console.log('Power On (P) sent'));
+	checkShairport();
 }
 
 function readSerialData(data) {
-  console.log(data.toString());
+	//console.log(data.toString());
 }
 
-function showPortClose() {
-  console.log('port closed.');
-}
+function showPortClose() { console.log(' serial port closed.'); }
 
-function showError(error) {
-  console.log('Serial port error: ' + error);
-}
-
+function showError(error) { console.log('Serial port error: ' + error); }
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # AMPi-Service
 AMPi is a project that integrates a mains electricity power supply with an audio amplifier, an 4-channel audio relay switcher, input audio transformers, a Raspberry Pi with DAC, a ST7735 based TFT display, and an Arduino Nano (to control the Raspberry PI over serial and the built-in screen) to make a completely integrated digital audio solution that supports the latest high quality internet audio streaming and smart speaker AirPlay & Bluetooth 4.2 functionality over either wired Ethernet or Wi-Fi in one box.
 
-This repository is the service code for the Raspberry Pi. It currently only has alpha-level code for the Pandora Player, called **pandorasbox** that is controlled via the web browser, and not yet through the AMPi interface.
+This repository is the service code for the Raspberry Pi.
 
-Some recent experimentation with a Node.js app lives in the **AMPi-Node** folder.
+The Node.js app lives in the **AMPi-Node** folder.
 
-For the code running on  Arduino Nano, go to [AMPi-Display-Interface](https://github.com/bjaan/AMPi-Display-Interface) repository.
+It also has alpha-level code for the Pandora Player, called **pandorasbox** that is controlled via the web browser, and not yet through the AMPi interface.
+
+For the code running on the Arduino Nano, go to [AMPi-Display-Interface](https://github.com/bjaan/AMPi-Display-Interface) repository.
 
 First prototype (WIP) with Raspberry Pi Model 3 and an Arduino Nano
 
@@ -20,7 +22,7 @@ First prototype (WIP) with Raspberry Pi Model 3 and an Arduino Nano
 * Clear audio using build-in pre-amp and audio transformers, to allow full galvanic separation between the external audio equipment and AMPi
 * Computer-controlled interface that controls screen, the power of and the service running on the built-in audio streaming device
 * Built-in power supply
-* AirPlay playback using [**Shairport Sync**](https://github.com/mikebrady/shairport-sync).  AMPi identifies itself as a AirPlay network player where you can connect your iPhone or Mac to play music.  You can also use iTunes on Windows 10 to play music or route your audio to AMPi using [**TuneBlade**](http://www.tuneblade.com/)
+* AirPlay playback using [**Shairport Sync**](https://github.com/mikebrady/shairport-sync). AMPi identifies itself as a AirPlay network player, where your iPhone or iTunes on a Mac can be connected to, to play music.  You can also use iTunes on Windows 10 to play music through AirPlay or route your audio to AMPi using [**TuneBlade**](http://www.tuneblade.com/)
 * Bluetooth 4.2 Playback, this overrides everything and directly accessible after power-up
 * Pandora.com music player (WIP), using [Pianobar](https://github.com/PromyLOPh/pianobar)
 
